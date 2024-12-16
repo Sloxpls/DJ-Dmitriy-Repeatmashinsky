e8efab26c0eaac27bf8e803b950b7441bfbf2a11
Jim Sjöberg <h22jisjo@du.se>
2024-12-16 14:01:54 +0100

    async def on_ready(): change to work with linux file system

--- src/app.py
+++ src/app.py
@@ -1,15 +1,18 @@
-import importlib
+import sys
 import os
+import importlib
 
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 
+# Add the '/app' directory to the module search path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 load_dotenv()
 
 ELEVEN_API_KEY = os.getenv("ELEVEN_API_KEY")
 STEAM_API_KEY = os.getenv("STEAM_API_KEY")
-
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -27,15 +30,14 @@ async def on_ready():
     await bot.change_presence(activity=discord.Game(name="Программирский"))
     print(f"Logged in as {bot.user}")
 
-    # Dynamically find the absolute path to 'cogs' directory
-    base_dir = os.path.dirname(os.path.abspath(__file__))  # Path to app.py
+    # Dynamically determine the cogs directory
+    base_dir = os.path.dirname(os.path.abspath(__file__))
     cogs_dir = os.path.join(base_dir, "cogs")
 
-    # Load cogs
     for filename in os.listdir(cogs_dir):
         if filename.endswith(".py"):
             cog_name = filename[:-3]
-            module = importlib.import_module(f"src.cogs.{cog_name}")
+            module = importlib.import_module(f"cogs.{cog_name}")
             cog_class = getattr(module, f"{cog_name.capitalize()}Cog")
 
             # Check if the cog requires an API key
@@ -55,7 +57,6 @@ async def d(ctx):
                 await message.delete()
             except Exception as e:
                 print(f"Failed to delete a message: {e}")
-
 
 if __name__ == "__main__":
     token = os.getenv("DISCORD_BOT_TOKEN")
